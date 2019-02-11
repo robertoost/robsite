f320d29c8a5c95d4689734c0992e92a971d77a2b
Robert Oost <robert@springtree.nl>
2019-02-11 23:17:16 +0100

    feat(music): load playlists and fade in at the same time

--- src/app/pages/music/music.component.ts
+++ src/app/pages/music/music.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { Component, ViewEncapsulation, AfterViewInit, QueryList, ViewChildren, OnInit } from '@angular/core';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { skip, first, filter } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
   styleUrls: ['./music.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class MusicComponent implements OnInit {
+export class MusicComponent implements AfterViewInit {
+
+  @ViewChildren(PlaylistComponent) playlistElements: QueryList<PlaylistComponent>;
+
+  playlistsVisible: boolean;
+  public playlists = [
+    'https://open.spotify.com/embed/user/sponsrob/playlist/1CVz8rad4rySVAU3aYvh5x',
+    'https://open.spotify.com/embed/user/sponsrob/playlist/4Xvy2yD9FzwNppIjvX40Km',
+    'https://open.spotify.com/embed/user/sponsrob/playlist/7LZVkv8mmswubQfHDs3R3Z'
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.revealPlaylist();
   }
 
+  async revealPlaylist() {
+    const observables = this.playlistElements.map( playlist => playlist.loaded.pipe( filter( load => load ), first() ) );
+
+    const joined = await forkJoin( observables ).pipe( first() ).toPromise();
+    console.log( 'visible', joined );
+    this.playlistsVisible = true;
+  }
 }
